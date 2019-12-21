ec60384de87895cedeb0860b2bebd0eaf414cc6c
guoxin-gh <1357546124@qq.com>
2019-12-21 16:07:37 +0800

    左侧idea代码提交

---
+++ src/main/java/com/offcn/controller/LeftController.java
@@ -0,0 +1,11 @@
+package com.offcn.controller;
+
+/**
+ * @ClassName LeftController
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/12/21 16:06
+ * @Version 1.0
+ */
+public class LeftController {
+}
