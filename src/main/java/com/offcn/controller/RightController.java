07e26a99a2578f4509975921b0efd97d7561f942
guoxin-gh <1357546124@qq.com>
2019-12-21 16:00:07 +0800

    右侧idea代码提交

---
+++ src/main/java/com/offcn/controller/RightController.java
@@ -0,0 +1,11 @@
+package com.offcn.controller;
+
+/**
+ * @ClassName RightController
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/12/21 15:59
+ * @Version 1.0
+ */
+public class RightController {
+}
